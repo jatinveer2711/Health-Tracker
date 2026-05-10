9f209a194cf338782cb658ad6853de41ba69d8c6
jatinveer2711 <jatinveerc@gmail.com>
2026-05-10 17:36:18 +0530

    add urls

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -29,7 +29,7 @@ export default function Signup() {
 
     try {
       const res = await axios.post(
-        "https://health-tracker-l043.onrender.com/api/users/signup",
+        "http://localhost:5000/api/users/signup",
         formdata
       );
 
